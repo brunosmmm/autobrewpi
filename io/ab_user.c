8aa5097b30fc8fba3559a56c5f2f987f8c6ca43c
Bruno Morais <brunosmmm@gmail.com>
2016-07-25 13:01:59 -0300

    use interrupts for encoder

--- io/ab_user.c
+++ io/ab_user.c
@@ -1,10 +1,19 @@
 #include "mcp23016.h"
 #include "serv.h"
 #include "keypad.h"
+#include "encoder.h"
 #include <pthread.h>
 #include <unistd.h>
+#include <string.h>
 
 unsigned char stop = 0x00;
+unsigned char pin_states[16];
+
+void _iochange(unsigned char * states)
+{
+    memcpy(pin_states, states, sizeof(unsigned char)*16);
+    ENCODER_SetPinStates(pin_states);
+}
 
 int main(void)
 {
@@ -12,8 +21,9 @@ int main(void)
     pthread_t serv_thread;
     int ret = 0;
 
-    MCP_Init(0x20, 0x01);
+    MCP_Init(0x20, 0x01, (MCP_IOCHANGE)_iochange);
     KEYPAD_Init();
+    ENCODER_Init();
 
     //server thread
     ret = pthread_create(&serv_thread, 0x00, ABUSER_serve, (void*)(&stop));
@@ -23,8 +33,9 @@ int main(void)
     {
 
         KEYPAD_Cycle();
+        //ENCODER_Cycle();
 
-        usleep(10000);
+        usleep(100);
     }
 
     stop = 0x01;
