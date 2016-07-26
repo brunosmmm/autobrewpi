e887f37dcf0e3e6a2050fbd627d8eb40b9d7297d
Bruno Morais <brunosmmm@gmail.com>
2016-07-26 16:34:31 -0300

    port connection primitives, inherit all from port

--- vspace/__init__.py
+++ vspace/__init__.py
@@ -22,9 +22,24 @@ def _check_variable_dtype(self, wanted_type, value):
     if error:
         raise TypeError('invalid python type for variable type {}'.format(wanted_type))
 
-class VSpaceInput(object):
-    def __init__(self, var_type, initial_value = None):
-        self._dtype = var_type
+class VSpacePort(object):
+    def __init__(self, dtype, global_id=None):
+        self._dtype = dtype
+        self._global_id = global_id
+        self._parent = None
+
+    def set_parent(self, parent_object):
+        self._parent = parent_object
+
+    def set_global_port_id(self, port_id):
+        self._global_id = port_id
+
+    def get_global_port_id(self):
+        return self._global_id
+
+class VSpaceInput(VSpacePort):
+    def __init__(self, dtype, initial_value = None):
+        super(VSpaceInput, self).__init__(dtype)
         self._value = initial_value
         self._value_changed = False
 
@@ -50,9 +65,9 @@ class VSpaceParameter(VSpaceInput):
     def __init__(self, var_type, initial_value = None):
         super(VSpaceParameter, self).__init__(var_type, initial_value)
 
-class VSpaceOutput(object):
-    def __init__(self, var_type, initial_value = None):
-        self._dtype = var_type
+class VSpaceOutput(VSpacePort):
+    def __init__(self, dtype, initial_value = None):
+        super(VSpaceOutput, self).__init__(dtype)
         #ideally this differs from an input in the way
         #the values are updated and propagated
         self._stored_value = initial_value
@@ -63,15 +78,25 @@ class VSpaceOutput(object):
 
         self._stored_value = value
 
+    def get_stored_value(self):
+        return self._stored_value
+
 class VSpaceDriver(object):
 
     _AVAILABLE_FLAGS = ('inputs_changed')
 
+    _inputs = {}
+    _outputs = {}
+
     def __init__(self):
         self._gvarspace = None
         self._instance_name = None
-        self._inputs = {}
-        self._outputs = {}
+
+        #link ports to driver
+        for port_name, port_object in self._inputs.iteritems():
+            port_object.set_parent(self)
+        for port_name, port_object in self._outputs.iteritems():
+            port_object.set_parent(self)
 
         #flags
         self._flags = set()
@@ -100,7 +125,7 @@ class VSpaceDriver(object):
         """
         self._outputs[variable_name].set_value(new_value)
         #trigger change
-        self._gvarspace.trigger_output_change(self._instance_name, variable_name)
+        self._gvarspace.trigger_output_change(self._instance_name, variable_name, new_value)
 
     def get_input_value(self, variable_name):
         return self._inputs[variable_name].get_value()
@@ -110,6 +135,9 @@ class VSpaceDriver(object):
 
     def set_output_value(self, variable_name, value):
         self._output_value_change(variable_name, value)
+
+    def get_instance_name(self):
+        return self._instance_name
 
     def get_flags(self):
         """Reads flags; will get cleared
