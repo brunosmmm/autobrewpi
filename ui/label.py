1721196a680daa48754dd947ad4293e4366c538a
Bruno Morais <brunosmmm@gmail.com>
2016-08-23 15:09:50 -0300

    automatically calculate label width / height

--- ui/label.py
+++ ui/label.py
@@ -1,5 +1,8 @@
-from ui.element import UIElement
+from ui.element import UIElement, Coordinate
 from ui.instr import DrawInstruction, DrawInstructionGroup
+import re
+
+_FONT_NAME_REGEX = re.compile(r'([0-9]+)x([0-9]+)')
 
 class Label(UIElement):
 
@@ -16,6 +19,21 @@ class Label(UIElement):
             self.vjust = kwargs.pop('vjust')
         else:
             self.vjust = 'top'
+
+        #try to guess font size
+        m = re.match(_FONT_NAME_REGEX, self.font)
+        if m != None:
+            self._font_w = int(m.group(1))
+            self._font_h = int(m.group(2))
+        else:
+            self._font_w = None
+            self._font_h = None
+
+        if 'font_w' in kwargs:
+            self._font_w = kwargs.pop('font_w')
+        if 'font_h' in kwargs:
+            self._font_h = kwargs.pop('font_h')
+
         super(Label, self).__init__(*args, **kwargs)
 
     def get_text(self):
@@ -39,3 +57,28 @@ class Label(UIElement):
             )
 
         return [dwg]
+
+    #label anchors are calculated differently because the width/height are dynamic
+    #depending on the size of the text
+
+    @property
+    def w(self):
+        if self._font_w is None:
+            return None
+
+        return len(self.text)*self._font_w
+
+    @w.setter
+    def w(self, value):
+        pass
+
+    @property
+    def h(self):
+        if self._font_w is None:
+            return None
+
+        return self._font_h
+
+    @h.setter
+    def h(self, value):
+        pass
