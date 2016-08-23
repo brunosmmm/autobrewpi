cdcde5fdae8e684deb30ba621261652ff5507e5e
Bruno Morais <brunosmmm@gmail.com>
2016-08-23 15:10:23 -0300

    added radio group management and use radius

--- ui/radiobutton.py
+++ ui/radiobutton.py
@@ -1,5 +1,30 @@
 from ui.checkbox import CheckBox
 from ui.instr import DrawInstruction, DrawInstructionGroup
+import uuid
+
+class GroupError(Exception):
+    pass
+
+class RadioGroup(object):
+
+    def __init__(self):
+        self._registered_btns = {}
+        self._selected_btn = None
+
+    def register_btn(self, btn_uid, state_change_cb):
+        self._registered_btns[btn_uid] = state_change_cb
+
+    def select_btn(self, btn_uid):
+        if btn_uid not in self._registered_btns:
+            raise GroupError('uid not registered in this group')
+
+        self._selected_btn = btn_uid
+        for uid, cb in self._registered_btns:
+            if cb is not None:
+                if uid == self._selected_btn:
+                    cb('pressed')
+                else:
+                    cb('normal')
 
 class RadioButton(CheckBox):
     def __init__(self, **kwargs):
@@ -8,7 +33,27 @@ class RadioButton(CheckBox):
         else:
             self.group = None
 
+        #use radius, not w/h
+        r = kwargs.pop('r')
+        kwargs['w'] = 2*r
+        kwargs['h'] = 2*r
+
+        #generate an unique id
+        self._uid = uuid.uuid1()
+
+        #register in group
+        if self.group is not None:
+            self.group.register_btn(self._uid, self.set_state)
+
         super(RadioButton, self).__init__(**kwargs)
+
+    #override set_state
+    def set_state(self, state):
+        if self.state != state:
+            super(RadioButton, self).set_state(state)
+            if self.group is not None:
+                if state == 'pressed':
+                    self.group.select_btn(self._uid)
 
     def _get_drawing_instructions(self):
         dwg = DrawInstructionGroup(self.draw_prio)
