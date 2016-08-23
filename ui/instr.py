1d74e0d5bbe0c9466c96668c013f4190081d0d64
Bruno Morais <brunosmmm@gmail.com>
2016-08-23 13:37:20 -0300

    enable translation of drawing instructions

--- ui/instr.py
+++ ui/instr.py
@@ -1,8 +1,24 @@
+import re
+
+_COORDINATE_REGEX = re.compile(r'(x|y)([0-9]+)?')
 
 class DrawInstruction(object):
     def __init__(self, kind, **kwargs):
         self.kind = kind
         self.kwargs = kwargs
+
+    def translate(self, x, y):
+        """Translate draw coordinates using general regex
+        """
+        for kwarg_name, kwarg_val in self.kwargs.iteritems():
+            m = re.match(_COORDINATE_REGEX, kwarg_name)
+
+            if m != None:
+                if m.group(1) == 'x':
+                    self.kwargs[kwarg_name] = kwarg_val + x
+                elif m.group(1) == 'y':
+                    self.kwargs[kwarg_name] = kwarg_val + y
+
 
 class DrawInstructionGroup(object):
     def __init__(self, draw_prio=0):
@@ -17,3 +33,8 @@ class DrawInstructionGroup(object):
 
     def set_priority(self, priority):
         self._prio = priority
+
+    def translate(self, x, y):
+
+        for instr in self._instrlist:
+            instr.translate(x, y)
