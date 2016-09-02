2abee85be23751efbebfb975e8ff7a2e0ecb4d48
brunosmmm <brunosmmm@gmail.com>
2016-09-02 16:34:21 -0300

    added more methods

--- vspace/vinport.py
+++ vspace/vinport.py
@@ -11,3 +11,13 @@ class VirtualInput(VSpaceDriver):
     @rpccallable
     def get_value(self):
         return self.__Input
+
+    def set_value(self, value):
+        """If disconnected, allows to set value
+        """
+        if self._inputs['Input'].get_connected() is False:
+            self._inputs['Input'].set_value(value)
+
+    @rpccallable
+    def is_connected(self):
+        return self._inputs['Input'].get_connected()
